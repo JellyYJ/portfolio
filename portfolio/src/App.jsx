be245910741028d42f2b00f7aefc4a5204b0a7b1
JellyYJ <lyj10000h@gmail.com>
2024-01-04 17:05:13 +0000

    Remove unnecessary files

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -6,6 +6,7 @@ import AppLayout from "./ui/AppLayout";
 import AboutMe from "./pages/AboutMe";
 import ContactMe from "./pages/ContactMe";
 import Projects from "./pages/Projects";
+import Project from "./pages/Project";
 import PageNotFound from "./pages/PageNotFound";
 
 function App() {
@@ -13,15 +14,27 @@ function App() {
     <>
       <GlobalStyles />
       <BrowserRouter>
-        <Routes>
+        {/* <Routes>
           <Route element={<AppLayout />}>
             <Route index element={<Navigate replace to="aboutMe"></Navigate>} />
             <Route element={<AboutMe />} path="aboutMe" />
             <Route element={<Projects />} path="projects" />
+            <Route element={<Project />} path="projects/:project" />
             <Route element={<ContactMe />} path="contactMe" />
           </Route>
 
           <Route element={<PageNotFound />} path="*" />
+        </Routes> */}
+
+        <Routes>
+          <Route path="/" element={<AppLayout />}>
+            <Route index element={<Navigate replace to="aboutMe" />} />
+            <Route element={<AboutMe />} path="aboutMe" />
+            <Route element={<Projects />} path="projects" />
+            <Route element={<Project />} path="projects/:project" />
+            <Route element={<ContactMe />} path="contactMe" />
+          </Route>
+          <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
     </>
