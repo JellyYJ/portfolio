ad549f2c82c06fbc2496aead7727a6e32e411aee
JellyYJ <lyj10000h@gmail.com>
2024-02-14 11:53:15 +0000

    Split components

--- portfolio/src/ui/ProjectsList.jsx
+++ portfolio/src/ui/ProjectsList.jsx
@@ -1,8 +1,6 @@
-import { Link } from "react-router-dom";
 import styled from "styled-components";
 import projectsData from "../assets/data/projectData.json";
-import Heading from "./Heading";
-import TechStack from "./TechStack";
+import ProjectsListItem from "./ProjectsListItem";
 
 const ProjectsListContainer = styled.ul`
   display: flex;
@@ -18,91 +16,6 @@ const ProjectsListContainer = styled.ul`
   }
 `;
 
-const ProjectImage = styled.img`
-  position: relative;
-  width: 80rem;
-  height: auto;
-  border-radius: 1.5rem;
-  margin-right: 5rem;
-
-  @media (max-width: 1240px) {
-    width: 65rem;
-  }
-
-  @media (max-width: 1080px) {
-    width: 65rem;
-    margin-right: 0;
-    margin-bottom: 2rem;
-  }
-
-  @media (max-width: 795px) {
-    width: 100%;
-    margin-right: 0;
-    margin-bottom: 1rem;
-  }
-`;
-
-const ConstructionIcon = styled.img`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  width: 80px; /* Adjust size as needed */
-  height: auto;
-`;
-
-const ProjectListItem = styled.li`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  background-color: var(--color-grey-0);
-
-  ${Heading} {
-    position: relative;
-    overflow: hidden;
-
-    &:before {
-      content: "";
-      position: absolute;
-      bottom: 5px;
-      width: 0;
-      height: 2px;
-      background: linear-gradient(var(--color-grey-700), transparent);
-      transition: width 0.3s ease-in-out;
-    }
-  }
-
-  &:hover {
-    ${ProjectImage} {
-      transition: transform 0.3s ease-in-out;
-      box-shadow: 1rem 1rem 3rem var(--color-brown-100);
-      transform: translateY(-10px);
-    }
-
-    ${Heading} {
-      &:before {
-        width: 100%;
-      }
-    }
-  }
-
-  @media (max-width: 1080px) {
-    flex-direction: column;
-  }
-`;
-
-const ProjectDetails = styled.div`
-  max-width: 50rem;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 0.8rem;
-`;
-
-const ProjectDescription = styled.div`
-  font-size: 1.8rem;
-`;
-
 function ProjectsList() {
   const filteredProjects = projectsData.filter(
     (project) => project.type !== "mini"
@@ -111,28 +24,7 @@ function ProjectsList() {
   return (
     <ProjectsListContainer direction="column" maxWidth="120rem">
       {filteredProjects.map((project) => (
-        <Link
-          to={project.status === "Ongoing" ? "#" : `/projects${project.link}`}
-          key={project.name}
-          style={{
-            pointerEvents: project.status === "Ongoing" ? "none" : "auto",
-          }}
-        >
-          <ProjectListItem key={project.name}>
-            <ProjectImage src={project.img} alt={project.name} />
-            {project.status === "Ongoing" && (
-              <ConstructionIcon
-                src="/src/assets/construction_symbol.png"
-                alt="Under construction"
-              />
-            )}
-            <ProjectDetails>
-              <Heading as="h3">{project.name}</Heading>
-              <ProjectDescription>{project.summary}</ProjectDescription>
-              <TechStack techData={project.techStck} />
-            </ProjectDetails>
-          </ProjectListItem>
-        </Link>
+        <ProjectsListItem project={project} key={project.name} />
       ))}
     </ProjectsListContainer>
   );
---
+++ portfolio/src/ui/ProjectsListItem.jsx
@@ -0,0 +1,117 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import Heading from "./Heading";
+import TechStack from "./TechStack";
+// import { LuConstruction } from "react-icons/lu";
+
+const ProjectImage = styled.img`
+  position: relative;
+  width: 80rem;
+  height: auto;
+  border-radius: 1.5rem;
+  margin-right: 5rem;
+  filter: ${(props) => (props.ongoing ? "blur(2px)" : "none")};
+
+  @media (max-width: 1240px) {
+    width: 65rem;
+  }
+
+  @media (max-width: 1080px) {
+    width: 65rem;
+    margin-right: 0;
+    margin-bottom: 2rem;
+  }
+
+  @media (max-width: 795px) {
+    width: 100%;
+    margin-right: 0;
+    margin-bottom: 1rem;
+  }
+`;
+
+const ProjectListItemContainer = styled.li`
+  position: relative; /* Added to set the position context for absolute positioning */
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  background-color: var(--color-grey-0);
+
+  ${Heading} {
+    position: relative;
+    overflow: hidden;
+
+    &:before {
+      content: "";
+      position: absolute;
+      bottom: 5px;
+      width: 0;
+      height: 2px;
+      background: linear-gradient(var(--color-grey-700), transparent);
+      transition: width 0.3s ease-in-out;
+    }
+  }
+
+  &:hover {
+    ${ProjectImage} {
+      transition: transform 0.3s ease-in-out;
+      box-shadow: 1rem 1rem 3rem var(--color-brown-100);
+      transform: translateY(-10px);
+    }
+
+    ${Heading} {
+      &:before {
+        width: 100%;
+      }
+    }
+  }
+
+  @media (max-width: 1080px) {
+    flex-direction: column;
+  }
+`;
+
+const ProjectDetails = styled.div`
+  max-width: 50rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.8rem;
+`;
+
+const ProjectDescription = styled.div`
+  font-size: 1.8rem;
+`;
+
+// const ConstructionIcon = styled(LuConstruction)`
+//   position: absolute;
+//   top: 18rem;
+//   right: 30rem;
+//   font-size: 5rem;
+// `;
+
+function ProjectsListItem({ project }) {
+  return (
+    <Link
+      to={project.status === "Ongoing" ? "#" : `/projects${project.link}`}
+      style={{
+        pointerEvents: project.status === "Ongoing" ? "none" : "auto",
+      }}
+    >
+      <ProjectListItemContainer>
+        <ProjectImage
+          src={project.img}
+          alt={project.name}
+          ongoing={project.status === "Ongoing"}
+        />
+
+        <ProjectDetails>
+          <Heading as="h3">{project.name}</Heading>
+          <ProjectDescription>{project.summary}</ProjectDescription>
+          <TechStack techData={project.techStck} />
+        </ProjectDetails>
+      </ProjectListItemContainer>
+    </Link>
+  );
+}
+
+export default ProjectsListItem;
